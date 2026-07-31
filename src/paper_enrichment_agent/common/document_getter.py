aa4e865169ddbb68e22ca9d8fdceb59371459776
WiktorProsowicz <prosowicz.wiktor@gmail.com>
2026-07-31 13:19:31 +0000

    feat: Implement DocumentGetter class

---
+++ src/paper_enrichment_agent/common/document_getter.py
@@ -0,0 +1,95 @@
+"""Contains a class that retrieves stats and components from a parsed document.
+
+See :class:`~paper_enrichment_agent.common.models.document.Document` for the document structure.
+"""
+
+from collections.abc import Iterator
+
+from paper_enrichment_agent.common.models import document as doc_models
+
+
+class DocumentGetter:
+    """Retrieves stats and components from a parsed document.
+
+    See :class:`~paper_enrichment_agent.common.models.document.DocumentComponent` for the path
+        definition.
+    """
+
+    def __init__(self, document: doc_models.Document) -> None:
+        self._document = document
+
+    def get_path_of_component(self, target_component: doc_models.DocumentComponent) -> str:
+        """Returns the path of the specified component in the document.
+
+        Raises:
+            ValueError: If the component is not found in the document.
+        """
+
+        for section in self._document.sections:
+            for path, component in self._iter_section_components(section):
+                if component == target_component:
+                    return path
+
+        raise ValueError(f'Component not found in the document: {target_component}')
+
+    def get_component_by_path(self, target_path: str) -> doc_models.DocumentComponent:
+        """Returns the component at the specified path in the document.
+
+        Raises:
+            ValueError: If the component is not found in the document.
+        """
+
+        for section in self._document.sections:
+            for path, component in self._iter_section_components(section):
+                if path == target_path:
+                    return component
+
+        raise ValueError(f'Component not found in the document: {target_path}')
+
+    def iter_components_of_type[T: doc_models.DocumentComponent](
+        self, component_type: type[T]
+    ) -> Iterator[T]:
+        """Returns all components of the specified type from the document."""
+
+        for section in self._document.sections:
+            for _, component in self._iter_section_components(section):
+                if isinstance(component, component_type):
+                    yield component
+
+    def _iter_section_components(
+        self,
+        section: doc_models.Section,
+        root_path: str = '',
+    ) -> Iterator[tuple[str, doc_models.DocumentComponent]]:
+        """Recursively iterates over all components in a section.
+
+        For each component, yields a tuple containing the path to the component and the component.
+        """
+
+        section_path = f'{root_path}/{section.component_id}'
+
+        yield section_path, section
+
+        for component in section.components:
+            if isinstance(component, doc_models.Paragraph):
+                yield from self._iter_paragraph_components(component, section_path)
+
+            elif isinstance(component, doc_models.Section):
+                yield from self._iter_section_components(component, section_path)
+
+            else:
+                yield f'{section_path}/{component.component_id}', component
+
+    def _iter_paragraph_components(
+        self, paragraph: doc_models.Paragraph, root_path: str
+    ) -> Iterator[tuple[str, doc_models.DocumentComponent]]:
+        """Iterates over all DocumentComponent-type elements in a paragraph.
+
+        For each element, yields a tuple containing the path to the element and the element.
+        """
+
+        yield f'{root_path}/{paragraph.component_id}', paragraph
+
+        for element in paragraph.elements:
+            if isinstance(element, doc_models.DocumentComponent):
+                yield f'{root_path}/{paragraph.component_id}/{element.component_id}', element
