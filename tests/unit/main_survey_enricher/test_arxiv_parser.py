b2c8f8729f4d50500919a0addbc33a8bdbc08980
WiktorProsowicz <prosowicz.wiktor@gmail.com>
2026-08-06 12:37:46 +0000

    refactor: Move arxiv_parser.py to a separate `components` package

--- tests/unit/main_survey_enricher/test_arxiv_parser.py
+++ tests/unit/main_survey_enricher/test_arxiv_parser.py
@@ -6,7 +6,7 @@ import requests
 
 from paper_enrichment_agent.common.document_getter import DocumentGetter
 from paper_enrichment_agent.common.models import document as doc_models
-from paper_enrichment_agent.main_survey_enricher.arxiv_parser import ArxivParser
+from paper_enrichment_agent.main_survey_enricher.components.arxiv_parser import ArxivParser
 
 EXAMPLE_PAPER_1 = '1905.09263'
 EXAMPLE_PAPER_2 = '2006.04558'
@@ -50,7 +50,7 @@ def mock_example_paper(monkeypatch):
         return Mock(content=b'', raise_for_status=raise_for_status)
 
     monkeypatch.setattr(
-        'paper_enrichment_agent.main_survey_enricher.arxiv_parser.requests.get', fake_get
+        'paper_enrichment_agent.main_survey_enricher.components.arxiv_parser.requests.get', fake_get
     )
 
 
