9fd1d4ba720edbfcc555101c13e130046dee6c1d
WiktorProsowicz <prosowicz.wiktor@gmail.com>
2026-08-10 19:41:42 +0000

    feat: Introduce `DocDBClient`

--- src/paper_enrichment_agent/common/models/misc.py
+++ src/paper_enrichment_agent/common/models/misc.py
@@ -1,13 +1,45 @@
 """Contains miscellaneous data models that are shared by the app's services."""
 
+import uuid
 from typing import Annotated
 
 import pydantic
 from pydantic import Field
 
 
-class SurveyMetadata(pydantic.BaseModel):
-    """Represents the metadata of a survey added to the system."""
+class DocumentMetadata(pydantic.BaseModel):
+    """Represents the metadata of a document being in the system.
 
-    survey_id: Annotated[str, Field(description='The unique identifier of the survey.')]
-    survey_name: Annotated[str, Field(description='The name of the survey.')]
+    The `document` may be either a survey or a referenced paper.
+    """
+
+    paper_id: Annotated[str, Field(description='The unique identifier of the document.')] = (
+        uuid.uuid4().hex
+    )
+    name: Annotated[str, Field(description='The name of the document.')]
+    description: Annotated[str, Field(description='The description of the document.')]
+    images: Annotated[
+        dict[str, str],
+        Field(
+            description='A mapping of image paths to their paths in the database. The key is the'
+            'image path in the document, and the value is the path in the database.'
+        ),
+    ]
+
+
+class SurveyMetadata(pydantic.BaseModel):
+    """Represents the metadata of a survey being in the system."""
+
+    doc_metadata: Annotated[
+        DocumentMetadata, Field(description='The metadata of the survey document.')
+    ]
+    referenced_docs: Annotated[
+        dict[str, str],
+        Field(
+            description=(
+                'An id-to-id mapping of referenced documents in the survey.'
+                ' The key is the ID of the reference in the Document class,'
+                ' and the value is the `paper_id` of the referenced document.'
+            )
+        ),
+    ]
---
+++ src/paper_enrichment_agent/main_survey_enricher/components/doc_db_client.py
@@ -0,0 +1,123 @@
+"""Contains the client class that communicates with the document database."""
+
+import contextlib
+import json
+import uuid
+from collections.abc import Generator
+from typing import Any
+
+import requests
+from botocore.client import BaseClient as BotocoreBaseClient
+
+from paper_enrichment_agent.common.document_getter import DocumentGetter
+from paper_enrichment_agent.common.models import document as doc_models
+from paper_enrichment_agent.common.models.misc import DocumentMetadata, SurveyMetadata
+
+
+class DocDBClient:
+    """Handles read/write requests to the document database."""
+
+    class DocDBClientError(Exception):
+        """Base class for exceptions raised by the `DocDBClient`."""
+
+    def __init__(self, boto_client: BotocoreBaseClient) -> None:
+
+        self._boto_client = boto_client
+        self._s3_bucket = 'document_database'
+
+    def add_survey(self, document: doc_models.Document, name: str, description: str) -> None:
+        """Adds a survey to the document database."""
+
+        doc_metadata = DocumentMetadata(name=name, description=description, images={})
+
+        self._download_and_store_images(document=document, doc_metadata=doc_metadata)
+
+        survey_metadata = SurveyMetadata(doc_metadata=doc_metadata, referenced_docs={})
+
+        with self._get_db_json_reference(
+            f'documents/{doc_metadata.paper_id}/metadata.json', create_if_not_exists=True
+        ) as metadata_json:
+            metadata_json.update(survey_metadata.model_dump())
+
+    def _download_and_store_images(
+        self, document: doc_models.Document, doc_metadata: DocumentMetadata
+    ) -> None:
+        """Downloads and stores the images of a document in the database."""
+
+        doc_getter = DocumentGetter(document=document)
+
+        for image in doc_getter.iter_components_of_type(doc_models.ImgSubfigure):
+            image_extension = image.image_src.split('.')[-1]
+
+            image_db_id = uuid.uuid4().hex
+            image_db_path = (
+                f'documents/{doc_metadata.paper_id}/images/{image_db_id}.{image_extension}'
+            )
+
+            try:
+                with requests.get(image.image_src, stream=True) as response:
+                    response.raise_for_status()
+
+                    self._boto_client.upload_fileobj(
+                        response.raw,
+                        Bucket=self._s3_bucket,
+                        Key=image_db_path,
+                        ExtraArgs={
+                            'ContentType': response.headers.get(
+                                'Content-Type', 'application/octet-stream'
+                            )
+                        },
+                    )
+
+            except requests.RequestException as e:
+                raise self.DocDBClientError(
+                    f'Failed to download image from {image.image_src}: {e}'
+                ) from e
+
+            except Exception as e:
+                raise self.DocDBClientError(
+                    f'Failed to upload image to database at {image_db_path}: {e}'
+                ) from e
+
+            doc_metadata.images[doc_getter.get_path_of_component(image)] = image_db_path
+
+    @contextlib.contextmanager
+    def _get_db_json_reference(
+        self, path: str, create_if_not_exists: bool = False
+    ) -> Generator[dict[str, Any], None, None]:
+        """Returns a context manager that yields a JSON reference to a document in the database.
+
+        The context manager ensures that the JSON reference is properly synchronized with the
+        database.
+        """
+
+        try:
+            json_file = self._boto_client.get_object(Bucket=self._s3_bucket, Key=path)
+
+        except self._boto_client.exceptions.ClientError as e:
+            if e.response['Error']['Code'] == 'NoSuchKey' and create_if_not_exists:
+                self._boto_client.put_object(
+                    Bucket=self._s3_bucket, Key=path, Body=json.dumps({}).encode('utf-8')
+                )
+
+            else:
+                raise self.DocDBClientError(
+                    f'Failed to retrieve JSON from database at {path}: {e}'
+                ) from e
+
+        finally:
+            json_file = self._boto_client.get_object(Bucket=self._s3_bucket, Key=path)
+
+        try:
+            json_content = json.loads(json_file['Body'].read().decode('utf-8'))
+
+        except json.JSONDecodeError as e:
+            raise self.DocDBClientError(
+                f'Failed to decode JSON from database at {path}: {e}'
+            ) from e
+
+        yield json_content
+
+        self._boto_client.put_object(
+            Bucket=self._s3_bucket, Key=path, Body=json.dumps(json_content).encode('utf-8')
+        )
